9fb55f926a7d52209c992d87d1798bf9e0af1ca6
ThomasHoek <hoek.thomas@gmail.com>
2023-09-27 11:04:04 +0200

    file split parse completed

--- scripts/seperate_results.py
+++ scripts/seperate_results.py
@@ -5,7 +5,6 @@ from pathlib import Path
 # TODO: replace with parse data for produce
 broken = open("MED_NL/broken_num.txt", "r").readlines()
 broken = [int(x.rstrip()) for x in broken]
-print(broken)
 
 # get data
 # TODO: replace with parse data for produce
@@ -23,8 +22,7 @@ def to_print(x: str) -> str:
 
 
 # regex match and remove empty whitespace lines
-# FIXME: ADD PARSING FOR ERROR
-regex_match = r"(Inconsistency in node types \(entail\/8\)\||\d+:)"
+regex_match = r"(ERROR|Error|Inconsistency in node types \(entail\/8\)\||\d+:)"
 re_split = re.split(regex_match, all_info)
 re_split = [x for x in re_split if x.rstrip()]
 meta_data = to_print(re_split.pop(0))
@@ -38,10 +36,16 @@ re_split[-1] = re_split[-1].split(
 Path(f"{analysis_path}").mkdir(parents=True, exist_ok=True)
 u_u = open(f"{analysis_path}/unknown_unknown.txt", "w+")
 u_y = open(f"{analysis_path}/unknown_yes.txt", "w+")
+u_n = open(f"{analysis_path}/unknown_no.txt", "w+")
+
 y_u = open(f"{analysis_path}/yes_unknown.txt", "w+")
 y_y = open(f"{analysis_path}/yes_yes.txt", "w+")
+y_n = open(f"{analysis_path}/yes_no.txt", "w+")
+
 d_a = open(f"{analysis_path}/defected_aethel.txt", "w+")
 d_o = open(f"{analysis_path}/defected_other.txt", "w+")
+
+error_file = open(f"{analysis_path}/error.txt", "w+")
 
 
 # extract data
@@ -61,9 +65,7 @@ def get_data_from_log(sent: str) -> tuple[str, str, str]:
     return target, predict, other
 
 
-# for i in re_split[:10]:
-#     print(i)
-
+# set flags and size
 re_split_length = len(re_split)
 index_pointer = 0
 defected_flag = False
@@ -72,10 +74,16 @@ aethel_flag = False
 while index_pointer != re_split_length:
     defected_flag = False
     aethel_flag = False
+    error_flag = False
     prior_text = ""
 
     cur_line = re_split[index_pointer]
+
+    # error, inconsistant etc messages
     while not cur_line[:-1].isdigit():
+        if "error" in cur_line.lower():
+            error_flag = True
+
         if "Inconsistency" in cur_line:
             defected_flag = True
 
@@ -96,6 +104,7 @@ while index_pointer != re_split_length:
     cur_line = re_split[index_pointer]
     target, predict, other = get_data_from_log(cur_line)
 
+    # match to find correct file to write to
     match (defected_flag, target, predict):
         case (True, _, _):
             if aethel_flag:
@@ -108,19 +117,33 @@ while index_pointer != re_split_length:
         case (False, "unknown", "yes"):
             file = u_y
 
+        case (False, "unknown", "no"):
+            file = u_n
+
         case (False, "yes", "unknown"):
             file = y_u
 
         case (False, "yes", "yes"):
             file = y_y
 
-        case _:
-            raise NotImplementedError("Should not reachthis")
+        case (False, "yes", "no"):
+            file = y_n
 
+        case _:
+            print(defected_flag, target, predict)
+            raise NotImplementedError("Should not reach this")
+
+    # write all info to file
     file.write(to_print(prior_text))
     file.write(to_print(number_info))
     file.write(to_print(cur_line))
     file.write("\n")
 
+    # error tracking
+    if error_flag:
+        error_file.write(to_print(prior_text))
+        error_file.write(to_print(number_info))
+        error_file.write(to_print(cur_line))
+        error_file.write("\n")
+
     index_pointer += 1
-    print(index_pointer, re_split_length)
